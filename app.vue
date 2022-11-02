640318260fa44acfc09101626c2b22d8a3cd2e67
OthnielDona <othnieldona@gmail.com>
2022-11-02 18:44:53 +0100

    initial nuxt tailwindcss plus daisyUI setup

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1 class="text-6xl text text-red-500">
+      Nuxt Tailwindcss + daisyUI Boilerplate
+    </h1>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // coment if don't want to use daisyui
+    plugins: [require('daisyui')],
+  };
+  
